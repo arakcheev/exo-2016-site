27f5c49f88431b5fd5bf300ce5c1d8c0d9ac1977
Artem Arakcheev <artem.ft@gmail.com>
2016-04-05 14:13:54 +0300

    Fix Page title bug

--- app/assets/javascripts/common/directives/Header.js
+++ app/assets/javascripts/common/directives/Header.js
@@ -8,20 +8,6 @@ define(['angular'], function (angular) {
             templateUrl: "/assets/templates/header.html",
             link: function (scope, elem) {
 
-                scope.$on("$routeChangeSuccess", function (a, route) {
-                    try {
-                        var path = route.$$route.originalPath;
-                        var menuItem = scope.menu[path];
-                        if (menuItem !== undefined) {
-                            document.title = menuItem + " | Exo-2016";
-                        } else {
-                            document.title = "Welcome to Exo-2016";
-                        }
-                    } catch (e) {
-                        console.error("Error update page title. ", e);
-                    }
-                });
-
                 scope.menu = {
                     "/announcement": "Announcement",
                     "/program": "Program",
@@ -29,6 +15,18 @@ define(['angular'], function (angular) {
                     "/registration": "Registration",
                     "/contacts": "Contacts"
                 };
+
+                try {
+                    var path = $location.path();
+                    var menuItem = scope.menu[path];
+                    if (menuItem !== undefined) {
+                        document.title = menuItem + " | Exo-2016";
+                    } else {
+                        document.title = "Welcome to Exo-2016";
+                    }
+                } catch (e) {
+                    console.error("Error update page title. ", e);
+                }
 
                 scope.$active = function (url) {
                     return $location.path().indexOf(url) >= 0;
