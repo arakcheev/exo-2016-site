5f899fcbf7a1bc231d7f51bd1273b1e105c4ccba
Artem Arakcheev <artem.ft@gmail.com>
2016-04-07 11:34:10 +0300

    Announcement in pdf format link

--- app/assets/javascripts/modules/index/controllers/IndexController.js
+++ app/assets/javascripts/modules/index/controllers/IndexController.js
@@ -2,6 +2,9 @@ define(function () {
 
     function IndexController() {
 
+        this.go = function () {
+            window.location.href = "/assets/files/announcement.pdf";
+        };
     }
 
     return IndexController;
